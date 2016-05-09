520c44794801a973262a61fb3938d4e67747f7ad
[Alex] <aedoler@gmail.com>
2016-05-08 22:08:05 -0300

    update

--- recursion.py
+++ recursion.py
@@ -19,7 +19,25 @@ def gcd(a, b):
     return gcd(b, a % b)
 
 def compareTo(s1, s2):
-    
+    """Compares two strings to check if they are the same length and whether one is longer
+        than the other"""
+    move_slice1 = 0
+    move_slice2 = 1
+    if s1[move_slice1:move_slice2] == '' and s2[move_slice1:move_slice2] == '':
+        return 0 # return 0 if same length
+    elif s1[move_slice1:move_slice2] == '' and s2[move_slice1:move_slice2] != '':
+        return len(s2) * -1 # return negative number if s2 > s1
+    elif s1[move_slice1:move_slice2] != '' and s2[move_slice1:move_slice2] == '':
+        return len(s1) # return positive number if s1 > s2
+    else:
+        move_slice1 += 1 # with each new call, the next object in the string is checked if empty or not
+        move_slice2 += 1
+
+    return compareTo(s1[1:], s2[1:])
+
+
+
+
 
 
 
